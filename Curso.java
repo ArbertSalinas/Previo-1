ace5aa683f24b6379f049d2540cc6cb49be6792f
ArbertSalinas <arbertarmandosf@ufps.edu.co>
2024-06-14 16:49:43 -0500

    Create Curso.java

---
+++ Curso.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+class Curso {
+    String nombre;
+    int horas;
+
+    public Curso(String nombre, int horas) {
+        this.nombre = nombre;
+        this.horas = horas;
+    }
+}
