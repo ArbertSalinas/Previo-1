f9b441c088ccfbbf62a23a1c73e54976fa89eaa2
ArbertSalinas <arbertarmandosf@ufps.edu.co>
2024-06-14 16:51:37 -0500

    DepartamentoAcademico.java

---
+++ DepartamentoAcademico.java
@@ -0,0 +1,9 @@
+class DepartamentoAcademico {
+    String nombre;
+    Docente director;
+
+    public DepartamentoAcademico(String nombre, Docente director) {
+        this.nombre = nombre;
+        this.director = director;
+    }
+}
